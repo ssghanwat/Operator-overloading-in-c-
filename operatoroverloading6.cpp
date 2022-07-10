17e1566c5e9c7c6373f3afcd0f0436e4b3551372
sandeep ghanwat <101173367+ssghanwat@users.noreply.github.com>
2022-07-11 00:19:12 +0530

    Add files via upload

---
+++ operatoroverloading6.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+	public:
+		int x;
+		int y;
+		
+		Demo(int i=0,int j=0)
+		{
+			x=i;
+			y=j;
+		}		
+};
+
+Demo operator ++(Demo op)        //pre
+{
+	op.x++;
+	op.y++;
+	return op;
+}
+
+Demo operator ++(Demo &op,int)   //post
+{
+	op.x++;
+	po.y++;
+	cout<<"Inside Second\n";
+	return Demo(op.x-1,op.y-1);
+}
+
+int main()
+{
+	Demo obj1(10,20);
+	Demo obj2(10,20);
+	
+	Demo robj1(0,0);
+	Demo robj2(0,0);
+	
+	robj1 = ++obj1;             //without dummy
+	cout<<obj1.x<<" "<<obj.y<<"\n";             //11 21
+	cout<<robj1.x<<" "<<robj.y<<"\n";           //11 21
+	
+	robj2 = obj2++;           //with dummy
+	cout<<obj2.x<<" "<<obj2.y<<"\n";              //11 21
+	cout<<robj2.x<<" "<<robj2.y<<"\n";            //10 20
+	
+	return 0;
+}
